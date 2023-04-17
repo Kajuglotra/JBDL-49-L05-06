f25b3052bafe21b258fef4ddcf42904460b0129c
Kajuglotra <51613822+Kajuglotra@users.noreply.github.com>
2023-04-17 10:41:24 +0530

    deleting the old derectory

--- JBDL-L05-L06/src/geeks/stream/FunctionalInterfaceEx.java
+++
@@ -1,6 +0,0 @@
-package geeks.stream;
-
-@FunctionalInterface
-public interface FunctionalInterfaceEx {
-    public void  abstractMethod(int x, int y);
-}
--- JBDL-L05-L06/src/geeks/thread/MyTask.java
+++
@@ -1,8 +0,0 @@
-package geeks.thread;
-
-public class MyTask implements Runnable{
-    @Override
-    public void run() {
-        System.out.println("I am inside the Mytask run method body");
-    }
-}
